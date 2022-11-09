3ffbb4db4ddb63f03b2d5c4c5f2771973d47f025
unknown <jin1991919@hotmail.com>
2022-11-09 21:33:44 +0900

    first commit

---
+++ client.go
@@ -0,0 +1,25 @@
+package cclient
+
+import (
+	"net/http"
+
+	"golang.org/x/net/proxy"
+
+	utls "github.com/refraction-networking/utls"
+)
+
+func NewClient(clientHello utls.ClientHelloID, proxyUrl ...string) (http.Client, error) {
+	if len(proxyUrl) > 0 && len(proxyUrl[0]) > 0 {
+		dialer, err := newConnectDialer(proxyUrl[0])
+		if err != nil {
+			return http.Client{}, err
+		}
+		return http.Client{
+			Transport: newRoundTripper(clientHello, dialer),
+		}, nil
+	} else {
+		return http.Client{
+			Transport: newRoundTripper(clientHello, proxy.Direct),
+		}, nil
+	}
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module client
+
+go 1.19
+
+require (
+	github.com/refraction-networking/utls v1.1.5
+	golang.org/x/net v0.2.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
